9db3704ab9210517c6b0ac14785eac865bb015af
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-01 21:52:12 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog 
+  def initalize (dog_name)
+    @dog_name = dog_name
+  end 
+  
+  def name(dog_name)
+    @dog_name=dog_name
+  end 
+  
+def breed(dog_breed)
+  @dog_breed = dog_breed
+end 
+

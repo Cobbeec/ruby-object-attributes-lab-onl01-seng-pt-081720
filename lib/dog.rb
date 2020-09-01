5a45625b8232c6d9f225296382eb040d01a49a3a
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-01 23:14:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,11 +6,11 @@ def name
   @name 
 end 
 
-def breed 
-   @breed
+def breed (breed)
+   @breed = breed
  end 
  
- def breed=(new_breed)
-   @breed = (new_breed)
+ def breed
+   @breed 
 end
 end 
